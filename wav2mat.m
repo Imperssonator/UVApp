9f6aadb8bb9201be648c5583da56265c69a75580
Nils <imperssonator@gmail.com>
2016-06-03 17:15:31 -0400

    removed displays

--- wav2mat.m
+++ wav2mat.m
@@ -12,7 +12,7 @@ function [Waves,RawAbs,Num] = wav2mat(FilePath)
 Waves = 0;
 RawAbs = 0;
 
-disp(FilePath)
+% disp(FilePath)
 fid = fopen(FilePath);
 if fid==-1
     disp('File does not exist')
@@ -23,7 +23,7 @@ Num = textscan(fid,'%s%s','Delimiter',',');
 fclose(fid);
 Num = {cellfun(@strfix,Num{1},'UniformOutput',false), cellfun(@strfix,Num{2},'UniformOutput',false)};
 
-disp(Num)
+% disp(Num)
 Waves = (str2num(Num{1}{13}):str2num(Num{2}{13}))';
 RawAbs = cellfun(@(x) str2num(x),Num{1}(14:14+length(Waves)-1),'UniformOutput',false);
 RawAbs = cell2mat(RawAbs);
